27163c551a70a1867a5a2a4036dfe4f5197256a8
Ramon Rasheed <ramonrash2@gmail.com>
2025-02-23 15:42:26 +0100

    adjust middleware to fit new services

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -8,20 +8,24 @@ export const authCheck = createMiddleware(async (c, next) => {
   try {
     // Verify JWT from Authorization header
     const token = c.req.header("Authorization")?.replace("Bearer ", "");
-    if (!token)
+    if (!token) {
       throw new Error("Authorization token is missing");
+    }
 
     // Decode and validate the token
     const payload = await verifyJWT(token);
-    const user = await getUserById(payload.sub);
+    const user = await getUserById({ id: payload.sub, withPassword: false });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
 
     // Attach user data to the context
     c.set("user", user);
 
     // Proceed to the next middleware or route handler
     await next();
-  }
-  catch (err) {
+  } catch (err) {
     logger.error(err);
     return c.json({ success: false, message: "Unauthorized" }, 401);
   }
